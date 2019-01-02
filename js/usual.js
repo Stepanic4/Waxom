0206d9c6bb14a243c4fbcd7d1de58a243d2ff874
Ivan <Zolotukhin.ivan.1@gmail.com>
2019-01-02 21:11:02 +0200

    changed script

--- js/usual.js
+++ js/usual.js
@@ -38,7 +38,7 @@ $(function () {
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html, body').animate({
-                    scrollTop: target.offset().top
+                    scrollTop: (target.offset().top - 67)
                 }, 1000);
                 return true;
             }
